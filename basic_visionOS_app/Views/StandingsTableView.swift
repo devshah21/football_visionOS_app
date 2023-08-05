d909176938b2bc4110450a147f8b4bbf4865d379
devshah21 <mail2devshah@gmail.com>
2023-08-05 18:43:49 -0400

    created basic app

---
+++ basic_visionOS_app/Views/StandingsTableView.swift
@@ -0,0 +1,23 @@
+//
+//  StandingsTableView.swift
+//  basic_visionOS_app
+//
+//  Created by Dev Shah on 2023-08-05.
+//
+
+import SwiftUI
+import XCAFootballDataClient
+
+struct StandingsTableView: View {
+    
+    let competition: Competition
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).navigationTitle(competition.name)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        StandingsTableView(competition:.defaultCompetitions[1])
+    }
+}
