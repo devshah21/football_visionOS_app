1d88082b45c7480f327da9501bd6faf39f00ee3a
devshah21 <mail2devshah@gmail.com>
2023-08-05 17:21:22 -0400

    Initial Commit

---
+++ basic_visionOS_app/ContentView.swift
@@ -0,0 +1,34 @@
+//
+//  ContentView.swift
+//  basic_visionOS_app
+//
+//  Created by Dev Shah on 2023-08-05.
+//
+
+import SwiftUI
+import RealityKit
+import RealityKitContent
+
+struct ContentView: View {
+    var body: some View {
+        NavigationSplitView {
+            List {
+                Text("Item")
+            }
+            .navigationTitle("Sidebar")
+        } detail: {
+            VStack {
+                Model3D(named: "Scene", bundle: realityKitContentBundle)
+                    .padding(.bottom, 50)
+
+                Text("Hello, world!")
+            }
+            .navigationTitle("Content")
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ basic_visionOS_app/basic_visionOS_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  basic_visionOS_appApp.swift
+//  basic_visionOS_app
+//
+//  Created by Dev Shah on 2023-08-05.
+//
+
+import SwiftUI
+
+@main
+struct basic_visionOS_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
