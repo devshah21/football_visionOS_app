6af76a246c3523511618344e5729df6ba1be8606
devshah21 <mail2devshah@gmail.com>
2023-08-07 19:59:48 -0400

    created fetch phase + standingtableobservable in /observable dir

---
+++ basic_visionOS_app/FetchPhase.swift
@@ -0,0 +1,8 @@
+//
+//  FetchPhase.swift
+//  basic_visionOS_app
+//
+//  Created by Dev Shah on 2023-08-07.
+//
+
+import Foundation
---
+++ basic_visionOS_app/Observables/StandingTableObservable.swift
@@ -0,0 +1,8 @@
+//
+//  StandingTableObservable.swift
+//  basic_visionOS_app
+//
+//  Created by Dev Shah on 2023-08-07.
+//
+
+import Foundation
