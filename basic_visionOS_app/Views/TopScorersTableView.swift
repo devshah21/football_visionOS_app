e5283f32dfb8bee2fd4186582c74c9a02034d089
devshah21 <mail2devshah@gmail.com>
2023-08-14 20:34:10 -0400

    uploaded crests + defined columns

--- basic_visionOS_app/Views/TopScorersTableView.swift
+++ basic_visionOS_app/Views/TopScorersTableView.swift
@@ -6,6 +6,7 @@
 //
 
 import SwiftUI
+import SVGImageSwiftUI
 import XCAFootballDataClient
 
 struct TopScorersTableView: View {
@@ -17,12 +18,44 @@ struct TopScorersTableView: View {
         Table(of: Scorer.self) {
             TableColumn("Pos") { scorer in HStack {
                 Text(scorer.posText).fontWeight(.bold).frame(minWidth: 20)
+                
+                if let crest = scorer.team.crest, crest.hasSuffix("svg") {
+                                        SVGImage(url: URL(string: crest)!, size: .init(width: 40, height: 40))
+                                            .frame(width: 40, height: 40)
+                                            .cornerRadius(20)
+                                    } else {
+                                        AsyncImage(url: URL(string: scorer.team.crest ?? "")) { phase in
+                                            switch phase {
+                                            case .success(let image): image.resizable()
+                                            default: Circle().foregroundStyle(Color.gray.opacity(0.5))
+                                            }
+                                        }.frame(width: 40, height: 40)
+                                    }
+                                 
+                                    Text(scorer.player.name)
+                
             }}.width(min: 264)
+            
+            TableColumn("Matches") { Text($0.playedMatchesText).frame(minWidth: 64) }
+                            .width(64)
+                        
+                        TableColumn("Goals") { Text($0.goalsText).frame(minWidth: 64) }
+                            .width(64)
+                        
+                        TableColumn("⌀ Goals") { Text($0.goalsPerMatchRatioText).frame(minWidth: 64) }
+                            .width(64)
+                        
+                        TableColumn("Assists") { Text($0.assistsText).frame(minWidth: 64) }
+                            .width(64)
+                        
+                        TableColumn("Pen.") { Text($0.penaltiesText).frame(minWidth: 64) }
+                            .width(64)
         } rows: {
             ForEach(vm.scorers ?? []) {
                 TableRow($0)
             }
         }
+        .foregroundStyle(.primary)
             .navigationTitle(competition.name + " Top Scorers").task(id: vm.selectedFilter.id) {
             await vm.fetchTopScorers(competition: competition)
         }
