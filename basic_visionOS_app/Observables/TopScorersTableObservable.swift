6d56c92fcfff62b2ff4a740e954967a2cd260617
devshah21 <mail2devshah@gmail.com>
2023-08-14 20:11:48 -0400

    fixed api bug for top scorers + displayed scorers by index

--- basic_visionOS_app/Observables/TopScorersTableObservable.swift
+++ basic_visionOS_app/Observables/TopScorersTableObservable.swift
@@ -16,4 +16,35 @@ class TopScorersTableObservable {
     var fetchPhase = FetchPhase<[Scorer]>.initial
     
     var scorers: [Scorer]? { fetchPhase.value }
+    
+    var selectedFilter = FilterOption.latest
+    var filterOptions: [FilterOption] = {
+        var date = Calendar.current.date(byAdding: .year, value: -4, to: Date())!
+        var options = [FilterOption]()
+        for i in 0..<3 {
+            if let nextYear = Calendar.current.date(byAdding: .year, value: 1, to: date) {
+                options.append(.year(Calendar.current.component(.year, from: nextYear)))
+                date = nextYear
+            }
+        }
+        options.append(.latest)
+        return options
+    }()
+    
+    func fetchTopScorers(competition: Competition) async {
+        fetchPhase = .fetching
+        do {
+            var scorers = try await client.fetchTopScorers(competitionId: competition.id, filterOption: selectedFilter)
+            scorers = scorers.enumerated().map { index, scorer in var scorer = scorer
+                scorer.pos = index + 1
+                return scorer
+            }
+            fetchPhase = .success(scorers)
+            
+        } catch {
+            if Task.isCancelled { return }
+            fetchPhase = .failure(error)
+        }
+    }
 }
+
