6c08e7893bd92f6cc01408760a46cfc7c44a2ee0
devshah21 <mail2devshah@gmail.com>
2023-08-14 21:29:00 -0400

    project update

---
+++ basic_visionOS_app/Views/TopScorersTabItemView.swift
@@ -0,0 +1,18 @@
+//
+//  TopScorersTabItemView.swift
+//  basic_visionOS_app
+//
+//  Created by Dev Shah on 2023-08-14.
+//
+
+import SwiftUI
+
+struct TopScorersTabItemView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopScorersTabItemView()
+}
