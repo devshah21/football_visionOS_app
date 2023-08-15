b001c021dda28a4fbfd1f5765869fd413dcc09eb
devshah21 <mail2devshah@gmail.com>
2023-08-14 21:29:51 -0400

    created tabitemview for top scorers

--- basic_visionOS_app/Views/TopScorersTabItemView.swift
+++ basic_visionOS_app/Views/TopScorersTabItemView.swift
@@ -6,10 +6,24 @@
 //
 
 import SwiftUI
+import XCAFootballDataClient
 
 struct TopScorersTabItemView: View {
+    @State var selectedCompetition: Competition?
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationSplitView {
+            List(Competition.defaultCompetitions, id: \.self, selection: $selectedCompetition) {
+                Text($0.name)
+            }.navigationTitle("XCA Scorers")
+        } detail: {
+            if let selectedCompetition {
+                TopScorersTableView(competition: selectedCompetition)
+                    .id(selectedCompetition)
+            } else {
+                Text("Select a competition")
+            }
+        }
     }
 }
 
