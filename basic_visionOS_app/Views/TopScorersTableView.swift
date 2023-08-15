b92830a9a61110180829d0aa897a3cf8fa7e50d2
devshah21 <mail2devshah@gmail.com>
2023-08-14 20:01:30 -0400

    .

---
+++ basic_visionOS_app/Views/TopScorersTableView.swift
@@ -0,0 +1,18 @@
+//
+//  TopScorersTableView.swift
+//  basic_visionOS_app
+//
+//  Created by Dev Shah on 2023-08-14.
+//
+
+import SwiftUI
+
+struct TopScorersTableView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TopScorersTableView()
+}
