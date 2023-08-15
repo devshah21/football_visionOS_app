ff651a2d5ecd3c02e0167eb2241c7bf5f78e4180
devshah21 <mail2devshah@gmail.com>
2023-08-14 20:26:05 -0400

    finished creating table view

--- basic_visionOS_app/Views/TopScorersTableView.swift
+++ basic_visionOS_app/Views/TopScorersTableView.swift
@@ -11,9 +11,21 @@ import XCAFootballDataClient
 struct TopScorersTableView: View {
     
     let competition: Competition
+    @Bindable var vm = TopScorersTableObservable()
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).navigationTitle(competition.name + " Top Scorers")
+        Table(of: Scorer.self) {
+            TableColumn("Pos") { scorer in HStack {
+                Text(scorer.posText).fontWeight(.bold).frame(minWidth: 20)
+            }}.width(min: 264)
+        } rows: {
+            ForEach(vm.scorers ?? []) {
+                TableRow($0)
+            }
+        }
+            .navigationTitle(competition.name + " Top Scorers").task(id: vm.selectedFilter.id) {
+            await vm.fetchTopScorers(competition: competition)
+        }
     }
 }
 
