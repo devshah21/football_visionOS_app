1c3373f8ce623fa086a3918587a626b753c5bbd5
devshah21 <mail2devshah@gmail.com>
2023-08-14 20:21:10 -0400

    UI fix for top scorers

--- basic_visionOS_app/Views/TopScorersTableView.swift
+++ basic_visionOS_app/Views/TopScorersTableView.swift
@@ -13,7 +13,7 @@ struct TopScorersTableView: View {
     let competition: Competition
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/).navigationTitle(competition.name + " Top Scorers")
     }
 }
 
